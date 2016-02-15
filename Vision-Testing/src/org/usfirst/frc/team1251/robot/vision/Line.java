f05ac12984bd16f33974dc4d162229d216c666e2
Eric Engelhart <bob80333@gmail.com>
2016-02-15 16:42:31 -0500

    Move Lines and Points to the vision folder

--- Vision-Testing/src/org/usfirst/frc/team1251/robot/vision/Line.java
+++ Vision-Testing/src/org/usfirst/frc/team1251/robot/vision/Line.java
@@ -1,17 +1,22 @@
-package org.usfirst.frc.team1251.robot;
+package org.usfirst.frc.team1251.robot.vision;
 
 /**
  * 
  * @author Eric Engelhart
- * I made this class because I needed a basic Line object that used ints, not floats or doubles
- * The lines are used for representing the lines from GRIP to figure out which ones are targets
+ * I made this class because I needed a basic Line object that supported angle and angle as well as points
  */
 public class Line {
+	
 	private Point point1;
 	private Point point2;
-	public Line(Point point1, Point point2){
+	private double angle;
+	private double length;
+	
+	public Line(Point point1, Point point2, double angle, double length){
 		this.point1 = point1;
 		this.point2 = point2;
+		this.angle = angle;
+		this.length = length;
 	}
 	
 	public Point getPoint1() {
@@ -22,6 +27,14 @@ public class Line {
 		return point2;
 	}
 	
+	public double getAngle() {
+		return angle;
+	}
+
+	public double getLength() {
+		return length;
+	}
+
 	public boolean hasPoint(Point point){
 		// return statement that does the stuff instead of an if #advanced
 		return (point1.equals(point) || point2.equals(point));
