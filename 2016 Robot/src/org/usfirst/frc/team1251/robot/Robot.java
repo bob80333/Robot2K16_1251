cbf0db249d101046e116064707c2447ab669acb0
Sparx030 <Sparx030@gmail.com>
2016-02-18 18:50:04 -0500

    Quick conceptual code
    This includes no documentation

--- 2016 Robot/src/org/usfirst/frc/team1251/robot/Robot.java
+++ 2016 Robot/src/org/usfirst/frc/team1251/robot/Robot.java
@@ -2,27 +2,46 @@
 package org.usfirst.frc.team1251.robot;
 
 import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.CounterBase.EncodingType;
 
 /**
  * Main code...
  */
 
 public class Robot extends IterativeRobot {
+	RobotDrive driveBase;
 	Joystick mainController, altController;
 	Victor mIntake, mShooter;
 	Compressor compressor;
 	Solenoid collectorArm;
 	DigitalInput collectorDetect;
-	Boolean detect;
+	Encoder Encoder;
+	AnalogPotentiometer Pot;
+	PIDController Pid;
+	
+	double lRev, rRev, revSpeed, lAxis, rAxis;
+	boolean detect;
 	
     public void robotInit() {
+    	driveBase = new RobotDrive(0, 1, 2, 3);
     	mainController = new Joystick(0);
     	altController = new Joystick(1);
     	collectorArm = new Solenoid(0);
     	collectorDetect = new DigitalInput(3);
-    	mIntake = new Victor(2);
-    	mShooter = new Victor(3);
+    	mIntake = new Victor(4);
+    	mShooter = new Victor(5);
     	detect = false;
+    	lRev = 0;
+    	rRev = 0;
+    	lAxis = mainController.getRawAxis(0);
+    	rAxis = mainController.getRawAxis(1);
+    	revSpeed = 0.5;
+    	Encoder = new Encoder(1, 2, true, EncodingType.k2X);
+    	Pot = new AnalogPotentiometer(2, 180, -0.27);
+    	Encoder.setDistancePerPulse(1.5);
+    	Encoder.setPIDSourceType(PIDSourceType.kRate);
+    	Pot.setPIDSourceType(PIDSourceType.kDisplacement);
+    	Pid = new PIDController(0, 0, 0, Encoder, mShooter);
     }
 
     public void autonomousInit() {
@@ -34,10 +53,21 @@ public class Robot extends IterativeRobot {
     }
     
     public void teleopInit() {
-    	
+    	Pid.setPID(0.00005, 0.0000005, 0.005);
+    	Pid.disable();
     }
 
     public void teleopPeriodic() {
+    	
+    	if (lAxis != 0) {
+    		lRev += revSpeed;
+    	}
+    	if (rAxis != 0) {
+    		rRev += revSpeed;
+    	}
+    	driveBase.tankDrive(lAxis * Math.abs(lRev), rAxis * Math.abs(rRev));
+    	
+    	
         if (mainController.getRawButton(1)) {
         	collectorArm.set(true);
         }
@@ -45,8 +75,15 @@ public class Robot extends IterativeRobot {
         	collectorArm.set(false);
         }
         
-        if (mainController.getRawButton(4) && detect == false) {
-        	if (collectorDetect.get() == false) {
+        if (altController.getRawButton(6)) {
+        	Pid.enable();
+        }
+        else {
+        	Pid.disable();
+        }
+        
+        if (mainController.getRawButton(4) && !detect) {
+        	if (!collectorDetect.get()) {
         	mIntake.set(0.5);
         	}
         	else {
@@ -54,17 +91,15 @@ public class Robot extends IterativeRobot {
         		detect = true;
         	}
         }
-        else if ((mainController.getRawButton(3) && detect == true)) {
-        	if (collectorDetect.get() == true) {
+        else if ((mainController.getRawButton(3) && detect)) {
+        	if (!collectorDetect.get()) {
             	mIntake.set(0.5);
             	}
             	else {
             		mIntake.set(0);
             		detect = false;
             	}
-        	
-        }
-        
+        }      
         
     }
 
