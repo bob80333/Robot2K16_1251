8eeb75f10ee5724e076e14db321f7b863727d51c
Sparx030 <Sparx030@gmail.com>
2016-02-19 18:34:14 -0500

    Switching controller values and fixing detector

--- 2016 Robot/src/org/usfirst/frc/team1251/robot/Robot.java
+++ 2016 Robot/src/org/usfirst/frc/team1251/robot/Robot.java
@@ -20,16 +20,17 @@ public class Robot extends IterativeRobot {
 	Encoder Encoder;
 	AnalogPotentiometer Pot;
 	PIDController Pid;
-	double lRev, rRev, revSpeed, lAxis, rAxis, k_RPM1, k_RPM2;
+	double lRev=0, rRev=0, lAxis, rAxis;
 	boolean detect;
 	
-    public void robotInit() {
-    	/** Changeable variable values*/
-    	revSpeed = 0.5;
-    	k_RPM1 = 2500;
-    	k_RPM2 = 5000;
-    	
-    	
+	final double /** Changeable constant values */
+			revSpeed = 0.5,	//Drive rev speed
+			k_RPM1 = 1000, 	//Low RPM speed
+			k_RPM2 = 2000,	//Mid 1 RPM speed
+			k_RPM3 = 3000, 	//Mid 2 RPM speed
+			k_RPM4 = 4000;	//High RPM speed
+	
+    public void robotInit() {    	
     	//Drive base using PWM 0, 1, 2, 3
     	driveBase = new RobotDrive(0, 1, 2, 3);
     	
@@ -37,22 +38,23 @@ public class Robot extends IterativeRobot {
     	driveController = new Joystick(0);
     	operatorController = new Joystick(1);
     	
+    	//Declaring variables to driver axis
+    	lAxis = driveController.getRawAxis(0);
+    	rAxis = driveController.getRawAxis(1);
+    	
     	//Solenoids using pneumatics slot 1, 2
     	collectorArm = new Solenoid(0);
     	
     	//Button detector using DIO 0
-    	collectorDetect = new DigitalInput(0);
+    	collectorDetect = new DigitalInput(0); 
     	
     	//Motors using PWM 4, 5
     	mCollector = new Victor(4);
     	mShooter = new Victor(5);
     	
-    	//Declaring variables to driver axis
-    	lAxis = driveController.getRawAxis(0);
-    	rAxis = driveController.getRawAxis(1);
-    	
     	//Encoder using DIO 0, 1, 2
     	Encoder = new Encoder(1, 2, true, EncodingType.k2X);
+    	
     	//Potentiometer using analog 3
     	Pot = new AnalogPotentiometer(3, 180, -0.27);
     	
@@ -61,13 +63,6 @@ public class Robot extends IterativeRobot {
     	Encoder.setPIDSourceType(PIDSourceType.kRate);
     	Pot.setPIDSourceType(PIDSourceType.kDisplacement);
     	Pid = new PIDController(0, 0, 0, Encoder, mShooter);
-    	
-    	//Boolean variables
-    	detect = false;
-    	
-    	//Double variables
-    	lRev = 0;
-    	rRev = 0;
     }
 
     public void autonomousInit() {
@@ -84,6 +79,9 @@ public class Robot extends IterativeRobot {
     }
 
     public void teleopPeriodic() {
+    	//collector detection
+    	detect = collectorDetect.get();
+    	
     	//Drive rev up
     	if (lAxis != 0) {
     		lRev += revSpeed;
@@ -96,46 +94,70 @@ public class Robot extends IterativeRobot {
     	driveBase.tankDrive(lAxis * Math.abs(lRev), rAxis * Math.abs(rRev));
     	
     	//Collector arm up and down
-        if (driveController.getRawButton(1)) {
+        if (driveController.getRawButton(3)) { //up
         	collectorArm.set(true);
         }
-        if (driveController.getRawButton(2)) {
+        if (driveController.getRawButton(2)) { //down
         	collectorArm.set(false);
         }
         
         //Shooter multi-RPM
-        if (operatorController.getRawButton(6)) {
+        if (operatorController.getRawButton(5)) { //Low
         	Pid.setSetpoint(((k_RPM1/60)*360)*1.5);
         	Pid.enable();
+        	if (operatorController.getRawButton(2) && detect) {
+            	mCollector.set(1);
+            }
+        	else {
+        		mCollector.set(0);
+        	}
         }
-        else if (operatorController.getRawButton(7)) {
-        	Pid.setSetpoint(((k_RPM1/60)*360)*1.5);
+        else if (operatorController.getRawButton(6)) { //Medium 1
+        	Pid.setSetpoint(((k_RPM2/60)*360)*1.5);
         	Pid.enable();
+        	if (operatorController.getRawButton(2) && detect) {
+            	mCollector.set(1);
+            }
+        	else {
+        		mCollector.set(0);
+        	}
         }
-        else {
+        else if (operatorController.getRawButton(6)) { //Medium 2
+        	Pid.setSetpoint(((k_RPM3/60)*360)*1.5);
+        	Pid.enable();
+        	if (operatorController.getRawButton(2) && detect) {
+            	mCollector.set(1);
+            }
+        	else {
+        		mCollector.set(0);
+        	}
+        }
+        else if (operatorController.getRawButton(6)) { //High
+        	Pid.setSetpoint(((k_RPM4/60)*360)*1.5);
+        	Pid.enable();
+        	if (operatorController.getRawButton(2) && detect) {
+            	mCollector.set(1);
+            }
+        	else {
+        		mCollector.set(0);
+        	}
+        }
+        else { //PID off
         	Pid.setSetpoint(0);
         	Pid.disable();
         }
         
         //Collector detection 
-        if (driveController.getRawButton(4) && !detect) {
-        	if (!collectorDetect.get()) {
+        if (operatorController.getRawButton(3) && !detect) { //for intake
         	mCollector.set(0.5);
-        	}
-        	else {
-        		mCollector.set(0);
-        		detect = true;
-        	}
         }
-        else if ((driveController.getRawButton(3) && detect)) {
-        	if (!collectorDetect.get()) {
-            	mCollector.set(0.5);
-            	}
-            	else {
-            		mCollector.set(0);
-            		detect = false;
-            	}
+        else if (operatorController.getRawButton(1)) { //for outtake 
+            mCollector.set(-0.5);
         }
+        else { //Collector off
+        	mCollector.set(0);
+        }
+        
         
         //SmartDashboard information
         SmartDashboard.putNumber("Motor Revolutions/M ", ((Encoder.getRate()/1.5)/360)*60);
