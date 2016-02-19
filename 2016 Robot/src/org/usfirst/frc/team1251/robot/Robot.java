8caf6fdf5b87c95f1bc0d701b088f5f25ee2fe6d
Sparx030 <Sparx030@gmail.com>
2016-02-19 17:04:34 -0500

    Creating documentation

--- 2016 Robot/src/org/usfirst/frc/team1251/robot/Robot.java
+++ 2016 Robot/src/org/usfirst/frc/team1251/robot/Robot.java
@@ -1,4 +1,3 @@
-
 package org.usfirst.frc.team1251.robot;
 
 import edu.wpi.first.wpilibj.*;
@@ -6,45 +5,69 @@ import edu.wpi.first.wpilibj.CounterBase.EncodingType;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
- * Main code...
+ * This is the main code for team 1251's 2016 robot.
+ * Do not make any edits to this code without explicit permission from 
+ * Jared Pilewski, Eric Engelhart, Mr.Elich, or Juan Coto
  */
 
 public class Robot extends IterativeRobot {
 	RobotDrive driveBase;
-	Joystick mainController, altController;
-	Victor mIntake, mShooter;
+	Joystick driveController, operatorController;
+	Victor mCollector, mShooter;
 	Compressor compressor;
 	Solenoid collectorArm;
 	DigitalInput collectorDetect;
 	Encoder Encoder;
 	AnalogPotentiometer Pot;
 	PIDController Pid;
-	
 	double lRev, rRev, revSpeed, lAxis, rAxis, k_RPM1, k_RPM2;
 	boolean detect;
 	
     public void robotInit() {
-    	driveBase = new RobotDrive(0, 1, 2, 3);
-    	mainController = new Joystick(0);
-    	altController = new Joystick(1);
-    	collectorArm = new Solenoid(0);
-    	collectorDetect = new DigitalInput(3);
-    	mIntake = new Victor(4);
-    	mShooter = new Victor(5);
-    	detect = false;
-    	lRev = 0;
-    	rRev = 0;
-    	lAxis = mainController.getRawAxis(0);
-    	rAxis = mainController.getRawAxis(1);
+    	/** Changeable variable values*/
     	revSpeed = 0.5;
+    	k_RPM1 = 2500;
+    	k_RPM2 = 5000;
+    	
+    	
+    	//Drive base using PWM 0, 1, 2, 3
+    	driveBase = new RobotDrive(0, 1, 2, 3);
+    	
+    	//Joystick for the driver and operator
+    	driveController = new Joystick(0);
+    	operatorController = new Joystick(1);
+    	
+    	//Solenoids using pneumatics slot 1, 2
+    	collectorArm = new Solenoid(0);
+    	
+    	//Button detector using DIO 0
+    	collectorDetect = new DigitalInput(0);
+    	
+    	//Motors using PWM 4, 5
+    	mCollector = new Victor(4);
+    	mShooter = new Victor(5);
+    	
+    	//Declaring variables to driver axis
+    	lAxis = driveController.getRawAxis(0);
+    	rAxis = driveController.getRawAxis(1);
+    	
+    	//Encoder using DIO 0, 1, 2
     	Encoder = new Encoder(1, 2, true, EncodingType.k2X);
-    	Pot = new AnalogPotentiometer(2, 180, -0.27);
+    	//Potentiometer using analog 3
+    	Pot = new AnalogPotentiometer(3, 180, -0.27);
+    	
+    	//PID decelerations
     	Encoder.setDistancePerPulse(1.5);
     	Encoder.setPIDSourceType(PIDSourceType.kRate);
     	Pot.setPIDSourceType(PIDSourceType.kDisplacement);
     	Pid = new PIDController(0, 0, 0, Encoder, mShooter);
-    	k_RPM1 = 2500;
-    	k_RPM2 = 5000;
+    	
+    	//Boolean variables
+    	detect = false;
+    	
+    	//Double variables
+    	lRev = 0;
+    	rRev = 0;
     }
 
     public void autonomousInit() {
@@ -61,28 +84,31 @@ public class Robot extends IterativeRobot {
     }
 
     public void teleopPeriodic() {
-    	
+    	//Drive rev up
     	if (lAxis != 0) {
     		lRev += revSpeed;
     	}
     	if (rAxis != 0) {
     		rRev += revSpeed;
     	}
+    	
+    	//Drive base movement
     	driveBase.tankDrive(lAxis * Math.abs(lRev), rAxis * Math.abs(rRev));
     	
-    	
-        if (mainController.getRawButton(1)) {
+    	//Collector arm up and down
+        if (driveController.getRawButton(1)) {
         	collectorArm.set(true);
         }
-        if (mainController.getRawButton(2)) {
+        if (driveController.getRawButton(2)) {
         	collectorArm.set(false);
         }
         
-        if (altController.getRawButton(6)) {
+        //Shooter multi-RPM
+        if (operatorController.getRawButton(6)) {
         	Pid.setSetpoint(((k_RPM1/60)*360)*1.5);
         	Pid.enable();
         }
-        else if (altController.getRawButton(7)) {
+        else if (operatorController.getRawButton(7)) {
         	Pid.setSetpoint(((k_RPM1/60)*360)*1.5);
         	Pid.enable();
         }
@@ -90,38 +116,28 @@ public class Robot extends IterativeRobot {
         	Pid.setSetpoint(0);
         	Pid.disable();
         }
-        if (altController.getRawButton(7)) {
-        	Pid.enable();
-        }
-        else {
-        	Pid.disable();
-        }
         
-        if (mainController.getRawButton(4) && !detect) {
+        //Collector detection 
+        if (driveController.getRawButton(4) && !detect) {
         	if (!collectorDetect.get()) {
-        	mIntake.set(0.5);
+        	mCollector.set(0.5);
         	}
         	else {
-        		mIntake.set(0);
+        		mCollector.set(0);
         		detect = true;
         	}
         }
-        else if ((mainController.getRawButton(3) && detect)) {
+        else if ((driveController.getRawButton(3) && detect)) {
         	if (!collectorDetect.get()) {
-            	mIntake.set(0.5);
+            	mCollector.set(0.5);
             	}
             	else {
-            		mIntake.set(0);
+            		mCollector.set(0);
             		detect = false;
             	}
         }
         
-        SmartDashboard.putNumber("Motor Recolutions/M ", ((Encoder.getRate()/1.5)/360)*60);
-        
-    }
-
-    public void testPeriodic() {
-    
-    }
-    
+        //SmartDashboard information
+        SmartDashboard.putNumber("Motor Revolutions/M ", ((Encoder.getRate()/1.5)/360)*60);
+    }   
 }
